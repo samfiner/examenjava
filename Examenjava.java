03f8b51994973b65c543630f7f429b8e76290b0c
LINA BAÑO SERRANO <linabaliserr@gmail.com>
2019-07-02 08:53:18 -0500

    examen

---
+++ Examenjava.java
@@ -0,0 +1,45 @@
+﻿package examenjava;
+import java.util.*;
+public class Examenjava {
+     public static void main(String[] args) {
+
+        Scanner leer = new Scanner(System.in);
+        ArrayList<String> nombre = new ArrayList<String>();
+        ArrayList<Float> velocidad = new ArrayList<Float>();
+        ArrayList<Float> tiempo = new ArrayList<Float>();
+        int opc = 1, mt=0;
+        float set=0;
+        
+        while (opc != 0) {           
+            System.out.print("Digite el nombre del estudiante: ");
+            nombre.add(leer.next());
+            System.out.print("Digite la velocidad del estudiante(0 para salir):");
+            opcion = leer.nextInt();
+            if(opc != 0){
+                velocidad.add((float) opcion);
+                tiempo.add(100 / velocidad.get(velocidad.size()-1));
+                set = set + tiempo.get(tiempo.size()-1);
+            }
+        }
+        
+       set = set / tiempo.size();
+        
+        int i=0, cont=0;
+        for(; i<velocidad.size(); i++){
+            
+            if(velocidad.get(i)>velocidad.get(mt)){
+                mt = i;
+            }
+            if(tiempo.get(i)>set){
+                cont++;
+            }
+            
+            System.out.println(nombre.get(i) + " " + velocidad.get(i) + " " + tiempo.get(i));
+        } 
+        
+        System.out.println("Alumno más veloz: " + nombre.get(mt) + "  "+"Tiempo: " + tiempo.get(mt) +"  "+"Velocidad: " + velocidad.get(mt));
+        System.out.println("Promedio: " + set);
+        System.out.println("Estudiantes por encima del promedio: " + cont);
+    }
+    
+}
